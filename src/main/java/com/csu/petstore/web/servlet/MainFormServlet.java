8045dd71eb0ebd9f7bacf713616188d00c17ff6e
Leecream <2523967486@qq.com>
2025-11-13 22:37:00 +0800

    11/13修改

---
+++ src/main/java/com/csu/petstore/web/servlet/MainFormServlet.java
@@ -0,0 +1,18 @@
+package com.csu.petstore.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class MainFormServlet extends HttpServlet {
+
+    private static final String MAIN_FORM= "/WEB-INF/jsp/catalog/main.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher(MAIN_FORM).forward(req,resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/common/bottom.jsp
@@ -0,0 +1,4 @@
+    This is bottom
+
+</body>
+</html>
